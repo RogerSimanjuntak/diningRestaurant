99f59ea19f33a91f68de140a914a7342efafd9a4
Roger Simanjuntak <arbination13@gmail.com>
2023-04-06 02:15:57 +0700

    update new customer

--- app/src/main/java/com/example/diningrestaurant/fragment/PemesenanFragment.java
+++ app/src/main/java/com/example/diningrestaurant/fragment/PemesenanFragment.java
@@ -1,5 +1,7 @@
 package com.example.diningrestaurant.fragment;
 
+import static com.example.diningrestaurant.modelXadapterItem.itemFood.judulItem;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +9,93 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.diningrestaurant.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PemesenanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PemesenanFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//    }
 
-    public PemesenanFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PemesenanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PemesenanFragment newInstance(String param1, String param2) {
-        PemesenanFragment fragment = new PemesenanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+String[] nomormeja = {"11", "22", "33", "44", "55", "66", "77", "88"};
+    Button addMenu;
+    LinearLayout CardForm;
+    AutoCompleteTextView autoCompleteTextViewItem;
+    AutoCompleteTextView autoCompleteTextViewMeja;
+    ArrayAdapter<String> adapterItems;
+    ArrayAdapter<String> adapterMeja;
+    int autoCompleteTextViewCount = 0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pemesenan, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_pemesenan, container, false);
+
+
+//        LIST MEJA
+    autoCompleteTextViewMeja=view.findViewById(R.id.autoComplete_meja);
+    adapterMeja =new ArrayAdapter<String>(view.getContext(),R.layout.list_item,nomormeja);
+    autoCompleteTextViewMeja.setAdapter(adapterMeja);
+    autoCompleteTextViewMeja.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String nomormeja = adapterView.getItemAtPosition(i).toString();
+            Toast.makeText(getContext(), "Kamu Memilih" + nomormeja, Toast.LENGTH_SHORT).show();
+        }
+    });
+//        List Item PEMBUKA
+
+//        autoCompleteTextViewItem = view.findViewById(R.id.autoComplete_item);
+//        adapterItems = new ArrayAdapter<String>(view.getContext(), R.layout.list_item,judulItem);
+//        autoCompleteTextViewItem.setAdapter(adapterItems);
+//        autoCompleteTextViewItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String judulItem = adapterView.getItemAtPosition(i).toString();
+//                Toast.makeText(getContext(), "Kamu Memilih" + judulItem, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//        List Item PENUTUP
+        addMenu = view.findViewById(R.id.buttonadd);
+        CardForm = view.findViewById(R.id.cardForm);
+        addMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addAutoCompleteTextView();
+            }
+        });
+
+        return view;
+    }
+    private void addAutoCompleteTextView(){
+        AutoCompleteTextView autoCompleteTextView =new AutoCompleteTextView(getContext());
+        autoCompleteTextView.setId(View.generateViewId());
+
+        autoCompleteTextView.setHint("Masukkan Makanan Yang sesuai");
+        adapterItems = new ArrayAdapter<>(getContext(), R.layout.list_item,judulItem);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String judulItem = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getContext(), "Kamu Memilih Item " + judulItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+        CardForm.addView(autoCompleteTextView);
+        autoCompleteTextViewCount++;
     }
 }
