ebb7b041c9e72d17f7483d8e5287b45ea51164e5
salmanramadansyahputra <salmanramadansy@gmail.com>
2023-04-05 16:29:31 +0700

    making kasir fragment 100%

--- app/src/main/java/com/example/diningrestaurant/modelXadapterKasir/adapterKasir.java
+++ app/src/main/java/com/example/diningrestaurant/modelXadapterKasir/adapterKasir.java
@@ -3,35 +3,41 @@ package com.example.diningrestaurant.modelXadapterKasir;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.diningrestaurant.MainActivity;
 import com.example.diningrestaurant.R;
-import com.example.diningrestaurant.fragment.KasirFragment;
-import com.example.diningrestaurant.pages;
 
 import java.util.ArrayList;
 
 public class adapterKasir extends RecyclerView.Adapter<adapterKasir.ViewHolder> {
 
     Context context;
+
     private Activity activity;
     ArrayList<kasirModel> parentItemArrayList;
     ArrayList<KasiritemModel> childItemArrayList;
+    ArrayList<kasiritempopup> popupItemArraylist;
+    Button cancelpayment, donepayment;
+    RecyclerView popup_rv;
+    TextView nomermeja, totalpayment;
 
-    public adapterKasir(ArrayList<kasirModel> parentItemArrayList, ArrayList<KasiritemModel> childItemArrayList) {
+
+
+    public adapterKasir(ArrayList<kasirModel> parentItemArrayList, ArrayList<KasiritemModel> childItemArrayList, ArrayList<kasiritempopup> popupItemArraylist) {
         this.activity = activity;
         this.parentItemArrayList = parentItemArrayList;
         this.childItemArrayList = childItemArrayList;
+        this.popupItemArraylist = popupItemArraylist;
     }
 
     @NonNull
@@ -43,6 +49,8 @@ public class adapterKasir extends RecyclerView.Adapter<adapterKasir.ViewHolder> 
         return new ViewHolder(view);
     }
 
+
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
@@ -51,16 +59,48 @@ public class adapterKasir extends RecyclerView.Adapter<adapterKasir.ViewHolder> 
         holder.nomormeja.setText(Integer.toString(parentItem.nomormeja));
 
         adapterKasiritem adapterMember = new adapterKasiritem(childItemArrayList);
+
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity);
+
+
         holder.nested_rv.setLayoutManager(linearLayoutManager);
         holder.nested_rv.setAdapter(adapterMember);
 
+
         holder.cardView.setOnClickListener(e->{
+            AlertDialog dialog;
             AlertDialog.Builder builder = new AlertDialog.Builder(e.getContext());
             View dialogView = LayoutInflater.from(e.getContext()).inflate(R.layout.popupkasir,null);
+
+            adapterKasirpopup adapterMemberr = new adapterKasirpopup(popupItemArraylist);
+            LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(activity);
+            kasirModel parentItemm = parentItemArrayList.get(position);
+            popup_rv = dialogView.findViewById(R.id.recviewitemkasirpopup);
+            popup_rv.setLayoutManager(linearLayoutManager1);
+            popup_rv.setAdapter(adapterMemberr);
+            totalpayment = dialogView.findViewById(R.id.totalpaymentkasirpopup);
+            totalpayment.setText("Total = Rp. 60.000,-");
+            nomermeja = dialogView.findViewById(R.id.nomormejapopup);
+            nomermeja.setText(Integer.toString(parentItemm.nomormeja));
+
             builder.setView(dialogView);
-            builder.show();
+            dialog = builder.create();
+
+            cancelpayment = dialogView.findViewById(R.id.button);
+            cancelpayment.setOnClickListener(f->{
+                dialog.dismiss();
+            });
+
+            donepayment = dialogView.findViewById(R.id.button2);
+            donepayment.setOnClickListener(f->{
+                Toast.makeText(dialog.getContext(), "pembayaran selesai dilakukan", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            });
+
+            dialog.show();
         });
+
     }
 
 
@@ -78,12 +118,12 @@ public class adapterKasir extends RecyclerView.Adapter<adapterKasir.ViewHolder> 
         RecyclerView nested_rv;
         CardView cardView;
 
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             nomormeja = itemView.findViewById(R.id.nomormejagrid);
             nested_rv = itemView.findViewById(R.id.recviewitemkasir);
             cardView = itemView.findViewById(R.id.reccardview);
-
         }
     }
 
